b8e6e770b66c914f9415a32565a832fd30b1220f
Sven Schwermer <sven@svenschwermer.de>
2021-04-03 20:09:44 +0200

    Read DS18B20 sensors

--- README.md
+++ README.md
@@ -8,11 +8,14 @@
 ## Schematics
 - BME280 SCL: GPIO22
 - BME280 SDA: GPIO21
-- DS1603L RX: UART2 RX GPIO16
+- DS1603L RX: UART2 RX (GPIO16)
 - CAN TX: GPIO5
 - CAN RX: GPIO4
+- DS18B20: GPIO15
 
 ## Dependencies
 - Adafruit BME280 Library (library manager)
 - https://github.com/ttlappalainen/NMEA2000/archive/f49a25ae7fd2ab5f5efcf95c78cecf9395c961cb.zip
 - https://github.com/ttlappalainen/NMEA2000_esp32/archive/b6ff3b745a9755a36c821eb21a3450b5269be5a8.zip
+- OneWire
+- DallasTemperature
--- nmea2k.ino
+++ nmea2k.ino
@@ -19,11 +19,26 @@
 // DS1603L
 #include "ds1603l.h"
 
+// DS18B20
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
+
 //*****************************************************************************
 
 Adafruit_BME280 bme; // I2C: SCL=GPIO22 SDA=GPIO21
 
 DS1603L ultrasonic(Serial2);
+
+OneWire one_wire(15); // GPIO15
+DallasTemperature ds18b20(&one_wire);
+uint8_t ds18b20_count = 0;
+tN2kTempSource ds18b20_src[] = {
+  N2kts_ExhaustGasTemperature,
+  N2kts_HeatingSystemTemperature,
+};
+uint8_t ds18b20_addr[ARRAY_SIZE(ds18b20_src)][8];
 
 int NodeAddress;         // To store last Node Address
 Preferences preferences; // Nonvolatile storage on ESP32 - To store LastDeviceAddress
@@ -49,6 +64,22 @@ void setup()
     Serial.println("Could not find a valid BME280 sensor, check wiring!");
 
   Serial2.begin(9600);
+
+  ds18b20.setWaitForConversion(false);
+  ds18b20.begin();
+  for (bool ok=true; ok && ds18b20_count < ARRAY_SIZE(ds18b20_addr); )
+  {
+    auto addr = ds18b20_addr[ds18b20_count];
+    ok = ds18b20.getAddress(addr, ds18b20_count);
+    if (ok)
+    {
+      Serial.printf("DS18B20 index=%d addr=%02x%02x%02x%02x%02x%02x%02x%02x\n",
+        ds18b20_count, addr[0], addr[1], addr[2], addr[3], addr[4], addr[5], addr[6], addr[7]);
+      ++ds18b20_count;
+    }
+  }
+  Serial.printf("DS18B20 count: %d\n", ds18b20_count);
+  ds18b20.requestTemperatures();
 
   // Reserve enough buffer for sending all messages.
   NMEA2000.SetN2kCANMsgBufSize(16);
@@ -134,6 +165,24 @@ void SendN2kPressure(void)
   NMEA2000.SendMsg(N2kMsg);
 }
 
+void HandleDS18B20(void)
+{
+  if (ds18b20.isConversionComplete())
+  {
+    ds18b20.requestTemperatures();
+
+    for (uint8_t i=0; i<ds18b20_count; ++i)
+    {
+      float tempC = ds18b20.getTempC(ds18b20_addr[i]);
+      Serial.printf("Temperature (DS18B20 #%d): %3.1f°C\n", i, tempC);
+
+      tN2kMsg N2kMsg;
+      SetN2kTemperature(N2kMsg, 0, 0, ds18b20_src[i], CToKelvin(tempC));
+      NMEA2000.SendMsg(N2kMsg);
+    }
+  }
+}
+
 void CheckSourceAddressChange()
 {
   int SourceAddress = NMEA2000.GetN2kSource();
@@ -176,6 +225,7 @@ void loop()
     temperature,
     humidity,
     pressure,
+    ds18b20,
   } state = ultrasonic;
   static uint32_t last_measurement = millis();
 
@@ -197,9 +247,14 @@ void loop()
       break;
     case pressure:
       SendN2kPressure();
+      state = ds18b20;
+      break;
+    case ds18b20:
+      HandleDS18B20();
       state = ultrasonic;
       break;
     }
+    last_measurement = millis();
   }
 
   NMEA2000.ParseMessages();
